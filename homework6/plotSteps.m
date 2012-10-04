620e744820394fc283457b14e48583e4aa580a6f
Geoffrey Ulman <ulman@metsci.com>
2012-10-03 22:38:29 -0400

    homework6

--- homework6/plotSteps.m
+++ homework6/plotSteps.m
@@ -1,4 +1,4 @@
-function z = plotSteps( func, xrange, yrange )
+function z = plotSteps( func, xrange, yrange, steps )
     % doesn't quite do what I need:
     % http://stackoverflow.com/questions/3784059/contour-plot-a-custom-function
     
@@ -7,10 +7,13 @@ function z = plotSteps( func, xrange, yrange )
     
     for i = 1:len
         for j = 1:len
-            z(i,j) = func( [ xrange(i) yrange(j) ] );
+            z(j,i) = func( [ xrange(i) yrange(j) ] );
         end
     end
     
     contour( xrange, yrange, z )
+    hold on
+    plot( steps(1,:), steps(2,:), '--', 'LineWidth', 1 )
+    hold off
 end
 
