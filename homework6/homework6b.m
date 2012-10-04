c193c54a2bfef2f66380574463eb98b14cab5a1d
Geoffrey Ulman <ulman@metsci.com>
2012-10-03 22:45:43 -0400

    homework6: improved plotting routine -- still are bugs with newton method

--- homework6/homework6b.m
+++ homework6/homework6b.m
@@ -1,5 +1,5 @@
 [xs iterations steps] = gradient( @func_1, @grad_hessian_func_1, [1 1]', 0.001, 0.1 );
-plotSteps( @func_1, -3:0.05:3, -3:0.05:3, steps );
+plotSteps( @func_1, -2:0.05:1, -1:0.05:1, steps );
 figure(2)
 [xs iterations steps] = newton( @func_1, @grad_hessian_func_1, [1 1]', 0.001, 0.1 );
-plotSteps( @func_1, -3:0.05:3, -3:0.05:3, steps );
+plotSteps( @func_1, -2:0.05:1, -1:0.05:1, steps );
--- homework6/plotSteps.m
+++ homework6/plotSteps.m
@@ -2,16 +2,15 @@ function z = plotSteps( func, xrange, yrange, steps )
     % doesn't quite do what I need:
     % http://stackoverflow.com/questions/3784059/contour-plot-a-custom-function
     
-    len = length( xrange );
-    z = zeros( len, len );
+    z = zeros( length( yrange ), length( xrange ) );
     
-    for i = 1:len
-        for j = 1:len
+    for i = 1:length( xrange )
+        for j = 1:length( yrange )
             z(j,i) = func( [ xrange(i) yrange(j) ] );
         end
     end
     
-    contour( xrange, yrange, z )
+    contour( xrange, yrange, z, 50 )
     hold on
     plot( steps(1,:), steps(2,:), '--', 'LineWidth', 1 )
     hold off
