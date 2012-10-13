4710fb479566ef16f62e7b7a389c2e908925041b
Geoffrey Ulman <ulman@metsci.com>
2012-10-13 14:09:39 -0400

    homework6: finished part b and c

--- homework6/homework6b.m
+++ homework6/homework6b.m
@@ -1,5 +1,13 @@
+
+% run gradient method and plot series of intermediate points
+
 [xs iterations steps] = gradient( @func_1, @grad_hessian_func_1, [1 1]', 0.001, 0.5 );
 plotSteps( @func_1, -2:0.05:1, -1:0.05:1, steps );
+xs
+
+% run newton's method and plot series of intermediate points
+
 figure(2)
 [xs iterations steps] = newton( @func_1, @grad_hessian_func_1, [1 1]', 0.001, 0.5 );
 plotSteps( @func_1, -2:0.05:1, -1:0.05:1, steps );
+xs
