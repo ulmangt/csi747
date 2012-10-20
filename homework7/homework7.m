04a804e8915cd78176598ea5c6b9d58a80c4cf53
Geoffrey Ulman <ulman@metsci.com>
2012-10-20 16:27:42 -0400

    homework7: added problem 2

--- homework7/homework7.m
+++ homework7/homework7.m
@@ -1,4 +1,9 @@
 % http://www.wolframalpha.com/input/?i=min+2*x1+-+3*x2+subject+to+x1%5E2+%2B+x2%5E2+%3D+25
-[xs iterations steps] = constrained_newton( @f_1, @df_1, @hf_1, @g_1, @dg_1, [ -2.773 4.160 ] );
+% solution: ( - 10 / sqrt( 13 ) , 15 / sqrt( 13 ) = ( -2.7735, 4.16025 )
+[xs iterations steps] = constrained_newton( @f_1, @df_1, @hf_1, @g_1, @dg_1, [ -2.7735 4.16025 ] );
 xs
 
+% http://www.wolframalpha.com/input/?i=minimize+x1%5E2%2B2*x1*x2%2Bx2%5E2+subject+to+3*x1%5E2%2Bx2%5E2%3D9
+% solution: ( -3/2, 3/2 ) and ( 3/2, -3/2 )
+[xs iterations steps] = constrained_newton( @f_2, @df_2, @hf_2, @g_2, @dg_2, [ -1.51, 1.51 ] );
+xs
