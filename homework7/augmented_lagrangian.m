0095382dbafd83779c3ea6f1391d6ddeb4f7209d
Geoffrey Ulman <ulman@metsci.com>
2012-10-20 22:06:39 -0400

    homework7: added modified version of newton code from homework6 for use in augmented_lagrangian.m

--- homework7/augmented_lagrangian.m
+++ homework7/augmented_lagrangian.m
@@ -1,4 +1,4 @@
-function [ xs iter steps ] = augmented_lagrangian( f, df, hf, g, dg, guess, epsilon, k )
+function [ xs iter steps ] = augmented_lagrangian( f, df, hf, g, dg, hg, guess, epsilon, k )
 %AUGMENTED_LAGRANGIAN Find minimum value of func subject to constraints
 
     % if no epsilon was provided, set a default
@@ -10,6 +10,9 @@ function [ xs iter steps ] = augmented_lagrangian( f, df, hf, g, dg, guess, epsi
     if nargin < 8
         k = 1;
     end
+    
+    % initial values for primal variable
+    x = guess;
     
     % number of constraints / dual variables
     dg_x_size = size( dg( x ) );
@@ -27,10 +30,32 @@ function [ xs iter steps ] = augmented_lagrangian( f, df, hf, g, dg, guess, epsi
     % iteration counter
     iter = 0;
     
-    while ( norm_g >= epsilon )
+    AL  = @(x,y,k)(f(x)-y*g(x)+(k/2.0)*norm(g(x))^2);
+    
+    % gradient of AL
+    gAL = @(x,y,k)(df(x)'-dg(x)'*y+k*dg(x)'*g(x));
+    
+    % a helper function to return the ith index of vector
+    subindex = @(vec,i)(vec(i));
+    % inner statement in first summation in hessian AL
+    isum1 = @(x,y,i)(subindex(hg(x),i)*y(i));
+    % first summation in hessian AL
+    sum1 = @(x,y)(sum(arrayfun(isum1,x*ones(nc,1),y*ones(nc,1),1:nc)));
+    % inner statement in second summation in hessian AL
+    isum2 = @(x,y,i)(subindex(hg(x),i)*subindex(g(x),i));
+    % second summation in hessian AL
+    sum2 = @(x,y)(sum(arrayfun(isum2,x*ones(nc,1),y*ones(nc,1),1:nc)));
+    % hessian of AL
+    hAL = @(x,y,k)(hf(x)-sum1(x,y)+k*sum2(x,y)+k*dg(x)'*g(x));
+    
+    AL(x,y,k)
+    gAL(x,y,k)
+    hAL(x,y,k)
+    
+    %while ( norm_g >= epsilon )
        
         
         
-    end
+    %end
 
 end
---
+++ homework7/newton.m
@@ -0,0 +1,61 @@
+function xs = newton( func, grad_func, hessian_func, guess, epsilon, eta )
+%NEWTON Find minimum value of func
+
+    % if no epsilon was provided, set a default
+    if nargin < 4
+        epsilon = 0.001;
+    end
+
+    % if no eta was provided, set a default
+    if nargin < 5
+        eta = 0.2;
+    end
+    
+    % set the current solution to the initial guess
+    xs = guess;
+    
+    % dimension of func domain
+    dim = length( xs );
+    
+    % evaluate the function, gradient, and hessian at x_s
+    grad = grad_func( xs );
+    hessian = hessian_func( xs );
+    fsx = func( xs );
+    
+    while norm( grad ) >= epsilon
+        
+        % initial value of lambda (normalization constant for hessian)
+        lambda_s = 0.0001;
+        
+        % while relaxed hessian is not positive definite, increase lambda_s
+        % (as determined by smallest eigenvalue being negative)
+        relaxed_hessian = hessian + lambda_s * eye( dim );
+        
+        while min( eig( relaxed_hessian ) ) < 0
+           lambda_s = lambda_s * 10;
+           relaxed_hessian = hessian + lambda_s * eye( dim );
+        end
+        
+        % solve linear system ( quadratic aproximation to function)
+        % to find next newton step
+        dxs = linsolve( relaxed_hessian, -grad );
+        
+        % initial value of step size
+        as = 1.0;
+        
+        % decrease step size until a sufficient decrease in function
+        % value is achieved (how much is sufficient is controlled by eta
+        while func( xs + as * dxs ) - fsx >= eta * as * grad' * dxs
+           as = as / 2.0; 
+        end
+        
+        % update guess
+        xs = xs + as * dxs;
+        
+        % evaluate the function, gradient, and hessian at x_s
+        grad = grad_func( xs );
+        hessian = hessian_func( xs );
+        fsx = func( xs );
+    end
+end
+
