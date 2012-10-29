acf3b6104d2150420f5e6585b085ed750f01242f
Geoffrey Ulman <ulman@metsci.com>
2012-10-29 02:59:59 -0400

    homework8: added results for part a and b

--- homework8/homework8.m
+++ homework8/homework8.m
@@ -1,2 +1,5 @@
 % problem 2 part a
 [x,y] = nonlinear_rescaling( @f_1, @df_1, @hf_1, @g_1, @dg_1, @hg_1, [ 0 0 ], 1e-7, 0.8, .01 )
+
+% problem 2 part b
+[x,y] = nonlinear_rescaling( @f_2, @df_2, @hf_2, @g_2, @dg_2, @hg_2, [ 0 1 ], 1e-7, 0.8, 1 )
