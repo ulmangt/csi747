0dd294c99191e95061974ed7745e07e21d489369
Geoffrey Ulman <ulman@metsci.com>
2012-11-10 23:45:04 -0500

    homework9: c and g constraints are flipped

--- homework9/hg_2.m
+++ homework9/hg_2.m
@@ -8,7 +8,7 @@ function [ lambda ] = hg_2( x, i )
                    [  0  0  0  0  0 ] ];
     elseif i == 2
         lambda = zeros(5);
-        lambda(1,1) = 2;
+        lambda(1,1) = -2;
     elseif i == 3
         lambda = zeros(5);
     end
--- homework9/homework9.m
+++ homework9/homework9.m
@@ -2,4 +2,4 @@
 [x,y,z] = rescaling_augmented_lagrangian( @f_1, @df_1, @hf_1, @g_1, @dg_1, @hg_1, @c_1, @dc_1, @hc_1, [ 0 ; 0 ; 0 ; 0 ], 1e-7, 0.1, 100 )
 
 % problem 2
-[x,y,z] = rescaling_augmented_lagrangian( @f_2, @df_2, @hf_2, @g_2, @dg_2, @hg_2, @c_2, @dc_2, @hc_2, [ 0 ; 0 ; 0 ; 0 ; 0 ], 1e-7, 0.1, 100 )
+[x,y,z] = rescaling_augmented_lagrangian( @f_2, @df_2, @hf_2, @c_2, @dc_2, @hc_2, @g_2, @dg_2, @hg_2, [ -1.5 ; -1.5 ; 2.5 ; 1 ; 1 ], 1e-7, 0.1, 100 )
