d965e568c037f0bda1e0d4cfb40a99c0ea4bdd08
Geoffrey Ulman <ulman@metsci.com>
2012-12-06 22:26:35 -0500

    final: fixed more indexing problems

--- final/dg_1.m
+++ final/dg_1.m
@@ -2,5 +2,5 @@ function [ ret ] = dg_1( a )
 %DG_1 gradient of inequality constraints
 % 0 <= ai
 % 0 <= C - ai
-    ret = [ eye( size(a) ) ; -eye( size( a ) ) ];
+    ret = [ eye( length(a) ) ; -eye( length( a ) ) ];
 end
